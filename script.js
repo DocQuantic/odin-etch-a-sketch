e3acc7a124a2f7b1742de4845f8fcce1f02ba5c4
William Magrini <35266773+DocQuantic@users.noreply.github.com>
2025-03-17 09:49:43 +0100

    Add randomize and darkening functionalities
    Randomize : the pixel color is randomized on hover
    Darken : the pixel color is darkened by 10% on hover
    It is possible to combine darkening and randomizing

--- script.js
+++ script.js
@@ -4,11 +4,56 @@ gridSize = 16
 gridContainer = document.querySelector("#grid");
 createPixelsGrid(gridSize);
 
+gridContainer.addEventListener("mouseover", (e) => {
+    if(isRandomColor && !isDarken){
+        randRed = Math.floor(Math.random()*256);
+        randGreen = Math.floor(Math.random()*256);
+        randBlue = Math.floor(Math.random()*256);
+        e.target.style.backgroundColor = `rgb(${randRed}, ${randGreen}, ${randBlue})`;
+        e.target.style.opacity = "1";
+    } else if(isDarken && !isRandomColor){
+        e.target.style.backgroundColor = "#f4a6b8";
+        currentOpacity = parseFloat(e.target.style.opacity);
+        e.target.style.opacity = `${currentOpacity + 0.1}`;
+    } else if(isDarken && isRandomColor){
+        randRed = Math.floor(Math.random()*256);
+        randGreen = Math.floor(Math.random()*256);
+        randBlue = Math.floor(Math.random()*256);
+        currentOpacity = parseFloat(e.target.style.opacity);
+        e.target.style.backgroundColor = `rgb(${randRed}, ${randGreen}, ${randBlue})`;
+        e.target.style.opacity = `${currentOpacity + 0.1}`;
+    } else if (!isDarken && !isRandomColor){
+        e.target.style.backgroundColor = "#f4a6b8";
+        e.target.style.opacity = "1";
+    }
+})
+
+resetButton = document.querySelector("#reset");
+resetButton.addEventListener("click", () => {
+    gridSize = prompt("Enter the ize of the new grid");
+    gridSize>100 ? gridSize=100 : gridSize;
+    deletePixelsGrid()
+    createPixelsGrid(gridSize);
+})
+
+randomButton = document.querySelector("#random-color");
+isRandomColor = randomButton.checked;
+randomButton.addEventListener("change", (e) => {
+    isRandomColor = randomButton.checked;
+})
+
+darkenButton = document.querySelector("#darken");
+isDarken = darkenButton.checked;
+darkenButton.addEventListener("change", (e) => {
+    isDarken = darkenButton.checked;
+})
+
 function createPixelsGrid(gridSize){
     for(let i=0; i<gridSize**2; i++){
         newPixel = document.createElement("div");
-        newPixel.classList.add("pixel");
-        newPixel.style.cssText = `width: ${containerWidth/gridSize}px; aspect-ratio: 1;`;
+        newPixel.style.width = `${containerWidth/gridSize}px`
+        newPixel.style.aspectRatio = "1";
+        newPixel.style.opacity = "0";
         newPixel.setAttribute("id", `pixel-${i}`);
         gridContainer.appendChild(newPixel);
     }
@@ -22,14 +67,3 @@ function deletePixelsGrid(){
         pixel.remove();
     }
 }
-
-gridContainer.addEventListener("mouseover", (e) => {
-    e.target.classList.add("hovered");
-})
-
-resetButton = document.querySelector("#reset");
-resetButton.addEventListener("click", () => {
-    gridSize = prompt("Enter the ize of the new grid");
-    deletePixelsGrid()
-    createPixelsGrid(gridSize);
-})
